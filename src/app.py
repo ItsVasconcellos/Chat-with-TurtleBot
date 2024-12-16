11b26b37ba31e55fb983c3e550c53b35de299d8f
ItsVasconcellos <fernando.asv@uol.com.br>
2024-12-15 23:35:34 -0300

    feat: adding chatbot feature

--- src/app.py
+++ src/app.py
@@ -3,13 +3,12 @@ import re
 import os
 
 intentions = {
-    'Secretária': 'Me direcionando para a secretária',
+    'Secretaria': 'Me direcionando para a secretária',
     'Laboratório': 'Indo para o laboratório ',
     '': ''
 }
 
-
-st.title("Welcome to the chatbot 3000!")
+st.title("Bem vindo ao chatbot3000!")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -18,3 +17,18 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # Get response from chatbot
+    response = intentions.get(prompt, "Não entendi o que você quis dizer")
+    
+    # Display chatbot response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    
+    st.session_state.messages.append({"role": "assistant", "content": response})
